8977d255fc4dd00a96babefcb8cea5afd04340e2
Jordan <jordanhayes3523@gmail.com>
2023-12-21 15:35:41 -0500

    updated 12/21 2

--- components/gallery/gallery.tab.tsx
+++ components/gallery/gallery.tab.tsx
@@ -26,7 +26,7 @@ const GalleryTab: React.FC<GalleryTabProps> = ({
                     </span>
                     <span
                         className={cn(
-                            "absolute inset-0 rounded-md ring-2 ring-offset-2",
+                            "absolute insert-0 rounded-md ring-2 ring-offset-2",
                             selected ? "ring-black" : "ring-transparent"
                         )}
                     />
